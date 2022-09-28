e54110a6577004fe4ce172ab7e335b5bdebcd23c
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-09-28 10:36:49 -0400

    Complete basics functionality added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection(
     },
 );
 
-const choices = ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+const choices = ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'Update employee managers']
 
 function main(){
     inquirer
@@ -39,6 +39,13 @@ function main(){
                     type: 'input',
                     name: 'title',
                     message: 'Enter a title for the department',
+                    validate: function(title){
+                        if(title.trim().length === 0){
+                            console.log(chalk.red(" Please enter a valid title"));
+                            return false;
+                        }
+                        return true;
+                       }
                 },
             ])
             .then((response) => {
@@ -51,23 +58,30 @@ function main(){
                    type: 'input',
                    name: 'title',
                    message: 'Enter a title for the role',
+                   validate: function(title){
+                    if(title.trim().length === 0){
+                        console.log(chalk.red(" Please enter a valid title"));
+                        return false;
+                    }
+                    return true;
+                   }
                },
                {
                 type: 'list',
-                name: 'department_id',
-                message: 'choose a department id for the role',
-                choices: allDepartments(),
+                name: 'department',
+                message: 'choose a department for the role',
+                choices: departmentTitles(),
             },
             {
                 type: 'input',
                 name: 'salary',
                 message: 'Enter a salary for the role',
                 validate: function(salary){
-                    if(Number(salary) === NaN){
-                        console.log(chalk.red("Please enter a number")); 
+                    if(containsAnyLetters(Number(salary))){
+                        console.log(chalk.red(" Please enter a number")); 
                         return false;
                     } else if(Number(salary) <= 10000 || Number(salary) >= 1000001){
-                        console.log(chalk.red("Please enter a salary between 10000-1000000"))
+                        console.log(chalk.red(" Please enter a salary between 10000-1000000"))
                         return false;
                     }
 
@@ -76,10 +90,91 @@ function main(){
             },
            ])
            .then((response) => {
-                
-               addARole(response.title, response.department_id, response.salary);
+            const sql = `SELECT id FROM DEPARTMENTS WHERE title = "${response.department}";`
+            db.query(sql, (err, rows) => {
+                if (err) {
+                  console.log(chalk.red(err.message));
+                  return;
+                }
+                addARole(response.title, rows[0].id, response.salary);
+              });
+               
            })
-      }
+      } else if(response.options === "add an employee"){
+        inquirer
+            .prompt([
+                    {
+                        type: 'input',
+                        name: 'first_name',
+                        message: 'Enter the first name for the employee',
+                        validate: function(first_name){
+                         if(first_name.trim().length === 0){
+                             console.log(chalk.red(" Please enter a valid name"));
+                             return false;
+                         }
+                         return true;
+                        }
+                    }, 
+                    {
+                        type: 'input',
+                        name: 'last_name',
+                        message: 'Enter the last name for the employee',
+                        validate: function(last_name){
+                         if(last_name.trim().length === 0){
+                             console.log(chalk.red(" Please enter a valid name"));
+                             return false;
+                         }
+                         return true;
+                        }
+                    }, 
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: 'Choose a role for the employee',
+                        choices: roleTitles()
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: 'Choose a manager for the employee',
+                        choices: employeeNames()
+                    },
+            ])
+            .then((response) => {
+               
+                const sql = `SELECT id FROM ROLES WHERE title = "${response.role}";`
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                      console.log(chalk.red(err.message));
+                      return;
+                    }
+                    const roleId = rows[0].id;
+                    console.log(`Inside DB query roleID${roleId}`);
+
+                    if(response.manager === "No Manager"){
+                        addAnEmployee(response.first_name.toUpperCase(), response.last_name.toUpperCase(), roleId, null)
+                    } else {
+                        console.log(typeof response.manager)
+                        const sql2 = `SELECT id FROM EMPLOYEES WHERE CONCAT(first_name, ' ',last_name) = '${response.manager}';`
+                        db.query(sql2, (err, rows1) => {
+                            if (err) {
+                              console.log(chalk.red(err.message));
+                              return;
+                            }
+                             const managerId = rows1[0].id;
+                             console.log(`Inside Db query managerID${managerId}`);
+                             addAnEmployee(response.first_name.toUpperCase(), response.last_name.toUpperCase(), roleId, managerId)
+                          });
+                    }
+                    
+
+                  });
+
+              console.log(roleId);
+              console.log(managerId);
+              addAnEmployee(response.first_name.toUpperCase(), response.last_name.toUpperCase(), );
+            })
+      } 
     });
 
 }
@@ -139,35 +234,96 @@ function addADepartment(department){
 
 function addARole(title, department_id, salary){
        
-            const sql = `INSERT INTO ROLES (title, department_id, salary) VALUES ('${title}', '${department_id}', '${salary}');`
+            const sql = `INSERT INTO ROLES (title, department_id, salary) VALUES ('${title.toUpperCase()}', ${department_id}, ${salary});`
         db.query(sql, (err, rows) => {
             if (err) {
               console.log(chalk.red(err.message));
                return;
             }
             console.log(chalk.green(`Succesfully added ${title} to Roles`));
-            main()
+            main();
           });
         
 }
 
-
-
-function allDepartments(){
-    const sql = "SELECT * FROM DEPARTMENTS;"
-    const departmentsArray = []
+function addAnEmployee(first_name, last_name, role_id, manager_id){
+    const sql = `INSERT INTO EMPLOYEES (first_name, last_name, role_id, manager_id) VALUES ('${first_name}', '${last_name}', ${role_id}, ${manager_id});`
     db.query(sql, (err, rows) => {
         if (err) {
-          console.log(json({ error: err.message }));
+          console.log(chalk.blue(err.message));
            return;
         }
-        for(let i = 0; i < rows.length; i++){
-            departmentsArray.push(rows[i].id);
-        }
+        console.log(chalk.green(`Succesfully added ${first_name} ${last_name} to Employees`));
+        main();
       });
-      return departmentsArray;
 }
 
 
 
+function departmentTitles(){
+    const sql = "SELECT title FROM DEPARTMENTS;"
+    const departmentTitles = [];
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log(chalk.red(err.message));
+            return;
+        }
+        for(let i = 0; i < rows.length; i++){
+            departmentTitles.push(rows[i].title);
+        }
+      });
+      return departmentTitles;
+}
 
+
+
+function roleTitles(){
+    const sql = "SELECT title FROM ROLES;"
+    const roleTitles = [];
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log(chalk.red(err.message));
+            return;
+        }
+        for(let i = 0; i < rows.length; i++){
+            roleTitles.push(rows[i].title);
+        }
+      });
+      return roleTitles;
+}
+
+
+
+function employeeNames(){
+    const sql = "SELECT * FROM EMPLOYEES;"
+    const employeeNames = [];
+    db.query(sql, (err, rows) => {
+        if(err){
+            console.log(chalk.red(err.message))
+            return;
+        } else {
+            for(let i = 0; i < rows.length; i++){
+                employeeNames.push(`${rows[i].first_name} ${rows[i].last_name}`);
+            }
+            employeeNames.push("No Manager");
+        }
+    })
+    return employeeNames;
+}
+
+function updateEmployeeManager(employeeId, managerID){
+    const sql = `UPDATE EMPLOYEES SET manager_id = ${managerId} WHERE id = ${employeeID};`
+    db.query(sql, (err, status) => {
+        if(err){
+            console.log(chalk.red(`Failed to update manager`));
+        } else {
+            console.log("Manager Update Succesfull")
+        }
+    })
+}
+
+
+
+function containsAnyLetters(str) {
+    return /[a-zA-Z]/.test(str);
+  }
